7b5685c8eeeb098e49ec808c353bf2f5324731d9
Craig Ludington <me@alpheus.me>
2015-08-17 09:57:42 -0500

    Refactor problem 82 - word-chains - as a single fn form

--- src/fourclojure/word_chains.clj
+++ src/fourclojure/word_chains.clj
@@ -11,41 +11,43 @@
 ;; Write a function which takes a sequence of words, and returns true if they can be arranged into one continous word chain,
 ;; and false if they cannot.
 
-(defn distance
-  ([s len-s t len-t]
-   (cond (zero? len-s) len-t
-         (zero? len-t) len-s
-         :else (min (+ (distance s (dec len-s) t len-t)        1)
-                    (+ (distance s len-s       t (dec len-t))  1)
-                    (+ (distance s (dec len-s) t (dec len-t))  (if (= (nth s (dec len-s))
-                                                                      (nth t (dec len-t)))
-                                                                 0
-                                                                 1)))))
-  ([s t] (distance s (count s) t (count t))))
+(def __
+  (fn
+    [words]
+    (letfn [(distance [s len-s t len-t]
+              (cond (zero? len-s) len-t
+                    (zero? len-t) len-s
+                    :else (min (+ (distance s (dec len-s) t len-t)        1)
+                               (+ (distance s len-s       t (dec len-t))  1)
+                               (+ (distance s (dec len-s) t (dec len-t))  (if (= (nth s (dec len-s))
+                                                                                 (nth t (dec len-t)))
+                                                                            0
+                                                                            1)))))
+            (close-enough? [xs ys] (= 1 (distance xs (count xs) ys (count ys))))
+            (graph [xs] (reduce into {} (map (fn [x] {x (set (filter (partial close-enough? x) xs))}) xs)))
+            (chain? [path]
+              (loop [p path]
+                (let [[a b] p]
+                  (cond (not b) path
+                        (close-enough? a b) (recur (rest p))
+                        :else false))))
 
-(defn close-enough? [xs ys] (= 1 (distance xs ys)))
+            (traverse [graph seen head]
+              {:pre [(map? graph) (vector? seen) (string? head)]}
+              (let [seen (conj seen head)
+                    candidates (remove (set seen) (graph head))]
+                (if (empty? candidates)
+                  seen
+                  (traverse graph seen (first candidates)))))
+            (traversals [g] (map (partial traverse g []) (keys g)))
+            (chains [g] (filter #(= (count (keys g)) (count %)) (traversals g)))
+            (has-chain? [words] (if (seq (chains (graph words))) true false))]
+      (has-chain? words))))
 
-(defn graph [xs] (reduce into {} (map (fn [x] {x (set (filter (partial close-enough? x) xs))}) xs)))
 
-(defn chain?
-  [path]
-  (loop [p path]
-    (let [[a b] p]
-      (cond (not b) path
-            (close-enough? a b) (recur (rest p))
-            :else false))))
 
-(defn traverse
-  [graph seen head]
-  {:pre [(map? graph) (vector? seen) (string? head)]}
-  (let [seen (conj seen head)
-        candidates (remove (set seen) (graph head))]
-    (if (empty? candidates)
-      seen
-      (traverse graph seen (first candidates)))))
 
-(defn traversals [g] (map (partial traverse g []) (keys g)))
 
-(defn chains [g] (filter #(= (count (keys g)) (count %)) (traversals g)))
 
-(defn has-chain? [words] (if (seq (chains (graph words))) true false))
+
+
