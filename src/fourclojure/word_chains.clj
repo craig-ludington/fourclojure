eddc174282701ecd986564519f5c13e635db02ee
Craig Ludington <me@alpheus.me>
2015-08-20 14:56:00 -0500

    Refactor into single fn named __
    * that's what the website needs to run the tests

--- src/fourclojure/word_chains.clj
+++ src/fourclojure/word_chains.clj
@@ -11,37 +11,41 @@
 ;; Write a function which takes a sequence of words, and returns true if they can be arranged into one continous word chain,
 ;; and false if they cannot.
 
-(defn distance
-  ([s len-s t len-t]
-   (cond (zero? len-s) len-t
-         (zero? len-t) len-s
-         :else (min (+ (distance s (dec len-s) t len-t)        1)
-                    (+ (distance s len-s       t (dec len-t))  1)
-                    (+ (distance s (dec len-s) t (dec len-t))  (if (= (nth s (dec len-s)) (nth t (dec len-t))) 0 1)))))
-  ([s t] (distance s (count s) t (count t))))
+(def __
+  (fn [set-of-words]
+    (letfn [(distance
+              ([s len-s t len-t]
+               (cond (zero? len-s) len-t
+                     (zero? len-t) len-s
+                     :else (min (+ (distance s (dec len-s) t len-t)        1)
+                                (+ (distance s len-s       t (dec len-t))  1)
+                                (+ (distance s (dec len-s) t (dec len-t))  (if (= (nth s (dec len-s)) (nth t (dec len-t))) 0 1)))))
+              ([s t] (distance s (count s) t (count t))))
+            (close-enough? [xs ys] (= 1 (distance xs ys)))
+            (graph [xs] (reduce into {} (map (fn [x] {x (set (filter (partial close-enough? x) xs))}) xs)))
+            (depth-first-traversal
+              [graph start]
+              (loop [result  [start]
+                     visited #{start}
+                     stack   (list start)]
+                (if (empty? stack)
+                  result
+                  (let [next (first (sort (remove visited (graph (peek stack)))))]
+                    (if next
+                      (recur (conj result next) (conj visited next) (conj stack next))
+                      result ;; Stop at first dead end -- don't pop the stack to visit everything (recur result visited (pop stack))
+                      )))))
+            (has-chain?
+              [words]
+              (let [g (graph words)
+                    wc (count words)
+                    pass #(= wc (count (depth-first-traversal g %)))]
+                (if (some pass words)
+                  true
+                  false)))]
+      (has-chain? set-of-words))))
 
-(defn close-enough? [xs ys] (= 1 (distance xs ys)))
+(def has-chain? __)
 
-(defn graph [xs] (reduce into {} (map (fn [x] {x (set (filter (partial close-enough? x) xs))}) xs)))
 
-(defn depth-first-traversal
-  [graph start]
-  (loop [result  [start]
-         visited #{start}
-         stack   (list start)]
-    (if (empty? stack)
-      result
-      (let [next (first (sort (remove visited (graph (peek stack)))))]
-        (if next
-          (recur (conj result next) (conj visited next) (conj stack next))
-          result ;; Stop at first dead end -- don't pop the stack to visit everything (recur result visited (pop stack))
-          )))))
 
-(defn has-chain?
-  [words]
-  (let [g (graph words)
-        wc (count words)
-        pass #(= wc (count (depth-first-traversal g %)))]
-    (if (some pass words)
-      true
-      false)))
--- test/fourclojure/word_chains_test.clj
+++ test/fourclojure/word_chains_test.clj
@@ -2,50 +2,6 @@
   (:require [clojure.test :refer :all]
             [fourclojure.word-chains :refer :all]))
 
-(deftest test-distance
-  (testing "0 for equal strings"
-    (is (= 0 (distance "" "")))
-    (is (= 0 (distance "a" "a")))
-    (is (= 0 (distance "abc" "abc"))))
-
-  (testing "insert one character"
-    (testing "at end"
-      (is (= 1 (distance "" "a")))
-      (is (= 1 (distance "a" "ab"))))
-    (testing "in the middle"
-      (is (= 1 (distance "ac" "abc"))))
-    (testing "at beginning"
-      (is (= 1 (distance "cat" "at")))))
-
-  (testing "delete one character"
-    (testing "at end"
-      (is (= 1 (distance "abc" "ab"))))
-    (testing "in the middle"
-      (is (= 1 (distance "abc" "ac"))))
-    (testing "at beginning"
-      (is (= 1 (distance "cat" "at")))))
-
-  (testing "substitute one character"
-    (testing "at end"
-      (is (= 1 (distance "cat" "car"))))
-    (testing "in the middle"
-      (is (= 1 (distance "cat" "cot"))))
-    (testing "at beginning"
-      (is (= 1 (distance "cat" "bat")))))
-
-  (testing "two letter changes"
-    (testing "at end"
-      (is (= 2 (distance "bit" "bob"))))
-    (testing "in the middle"
-      (is (= 2 (distance "bite" "tate"))))
-    (testing "at beginning"
-      (is (= 2 (distance "bit" "tat")))))
-
-  (testing "things I missed"
-    (is (= 1 (distance "oat" "coat")))
-    (is (= 1 (distance "cot" "coat")))
-    (is (= 2 (distance "coat" "hot")))))
-
 (deftest test-has-chain?
   (is (= true  (has-chain? #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"})))
   (is (= true  (has-chain? #{"spout" "do" "pot" "pout" "spot" "dot"})))
