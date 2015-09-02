14c379d0c3ac8d6a3fb64c94425ca195a1bb0e0f
Craig Ludington <me@alpheus.me>
2015-09-01 20:50:24 -0500

    longest-increasing-subsequence - convert to canonical fn form

--- src/fourclojure/longest_increasing_sub_seq.clj
+++ src/fourclojure/longest_increasing_sub_seq.clj
@@ -7,39 +7,40 @@
 ;; the one that occurs first. An increasing sub-sequence must have a
 ;; length of 2 or greater to qualify.
 
-(defn increasing? [s] (apply < s))
+(def __
+  (fn [s]
+    (letfn [(increasing? [s] (apply < s))
 
-(defn increasing-subseq
-  [s]
-  (loop [s s]
-    (if (seq s)
-      (if (increasing? s)
-        (vec s)
-        (recur (take (dec (count s)) s)))
-      [])))
+            (increasing-subseq
+              [s]
+              (loop [s s]
+                (if (seq s)
+                  (if (increasing? s)
+                    (vec s)
+                    (recur (take (dec (count s)) s)))
+                  [])))
 
-(defn all-increasing-subseqs
-  [s]
-  (loop [s s
-         acc []]
-    (if (seq s)
-      (recur (rest s)
-             (conj acc (increasing-subseq s)))
-      acc)))
+            (all-increasing-subseqs
+              [s]
+              (loop [s s
+                     acc []]
+                (if (seq s)
+                  (recur (rest s)
+                         (conj acc (increasing-subseq s)))
+                  acc)))
 
-(defn best-increasing-subseq
-  [s]
-  (loop [qualifiers (all-increasing-subseqs s)
-         best nil]
-    (if (seq qualifiers)
-      (recur (rest qualifiers)
-             (if (> (count (first qualifiers))
-                    (count best))
-               (first qualifiers)
-               best))
-      (if (> (count best) 1)
-        best
-        []))))
-
-(def __ best-increasing-subseq)
+            (best-increasing-subseq
+              [s]
+              (loop [qualifiers (all-increasing-subseqs s)
+                     best nil]
+                (if (seq qualifiers)
+                  (recur (rest qualifiers)
+                         (if (> (count (first qualifiers))
+                                (count best))
+                           (first qualifiers)
+                           best))
+                  (if (> (count best) 1)
+                    best
+                    []))))]
+      (best-increasing-subseq s))))
 
