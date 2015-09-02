522e2093c37cd0e95c4459ce43754e37e92de15c
Craig Ludington <me@alpheus.me>
2015-09-02 03:12:59 -0500

    tic-tac-toe in canonical fn form

--- src/fourclojure/tic_tac_toe.clj
+++ src/fourclojure/tic_tac_toe.clj
@@ -11,34 +11,32 @@
 ;; tic-tac-toe board and returns :x if X has won, :o if O has won, and
 ;; nil if neither player has won.
 
-(defn xy [b x y] ((b x) y))
-
-(defn candidates
-  [b]
-  (loop [coords [[[0 0] [1 0] [2 0]] ;; first column
-                 [[0 1] [1 1] [2 1]] ;; second column
-                 [[0 2] [1 2] [2 2]] ;; third column
-                 [[0 0] [1 1] [2 2]] ;; first diagonal
-                 [[2 0] [1 1] [0 2]] ;; second diagonal
-                 ]
-         b b]
-    (if (seq coords)
-      (recur (rest coords)
-             (let [make (fn [[c0 c1 c2]]
-                          [(apply (partial xy b) c0)
-                           (apply (partial xy b) c1)
-                           (apply (partial xy b) c2)])]
-               (conj b (make (first coords)))))
-      b)))
-
-(defn who-wins?
-  [boxes]
-  (let [p (first boxes)]
-    (and (every? #(= p %) boxes)
-         p)))
-
-(defn winner
-  [board]
-  (first (remove #(= :e %) (filter keyword? (map who-wins? (candidates board))))))
-
-(def __ winner)
+(def __
+  (fn [board]
+    (letfn [(xy [b x y] ((b x) y))
+            (candidates
+              [b]
+              (loop [coords [[[0 0] [1 0] [2 0]] ;; first column
+                             [[0 1] [1 1] [2 1]] ;; second column
+                             [[0 2] [1 2] [2 2]] ;; third column
+                             [[0 0] [1 1] [2 2]] ;; first diagonal
+                             [[2 0] [1 1] [0 2]] ;; second diagonal
+                             ]
+                     b b]
+                (if (seq coords)
+                  (recur (rest coords)
+                         (let [make (fn [[c0 c1 c2]]
+                                      [(apply (partial xy b) c0)
+                                       (apply (partial xy b) c1)
+                                       (apply (partial xy b) c2)])]
+                           (conj b (make (first coords)))))
+                  b)))
+            (who-wins?
+              [boxes]
+              (let [p (first boxes)]
+                (and (every? #(= p %) boxes)
+                     p)))
+            (winner
+              [board]
+              (first (remove #(= :e %) (filter keyword? (map who-wins? (candidates board))))))]
+      (winner board))))
