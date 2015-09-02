93e1df43ec4758f036f75dd475660fcfb6fc35a8
Craig Ludington <me@alpheus.me>
2015-09-02 03:07:36 -0500

    tic-tac-toe tests pass

---
+++ src/fourclojure/tic_tac_toe.clj
@@ -0,0 +1,44 @@
+(ns fourclojure.tic-tac-toe
+  (:use [clojure.pprint :refer :all]
+         [clojure.repl :refer :all]))
+
+;; http://www.4clojure.com/problem/73
+
+;; A tic-tac-toe board is represented by a two dimensional vector. X is
+;; represented by :x, O is represented by :o, and empty is represented
+;; by :e. A player wins by placing three Xs or three Os in a horizontal,
+;; vertical, or diagonal row. Write a function which analyzes a
+;; tic-tac-toe board and returns :x if X has won, :o if O has won, and
+;; nil if neither player has won.
+
+(defn xy [b x y] ((b x) y))
+
+(defn candidates
+  [b]
+  (loop [coords [[[0 0] [1 0] [2 0]] ;; first column
+                 [[0 1] [1 1] [2 1]] ;; second column
+                 [[0 2] [1 2] [2 2]] ;; third column
+                 [[0 0] [1 1] [2 2]] ;; first diagonal
+                 [[2 0] [1 1] [0 2]] ;; second diagonal
+                 ]
+         b b]
+    (if (seq coords)
+      (recur (rest coords)
+             (let [make (fn [[c0 c1 c2]]
+                          [(apply (partial xy b) c0)
+                           (apply (partial xy b) c1)
+                           (apply (partial xy b) c2)])]
+               (conj b (make (first coords)))))
+      b)))
+
+(defn who-wins?
+  [boxes]
+  (let [p (first boxes)]
+    (and (every? #(= p %) boxes)
+         p)))
+
+(defn winner
+  [board]
+  (first (remove #(= :e %) (filter keyword? (map who-wins? (candidates board))))))
+
+(def __ winner)
---
+++ test/fourclojure/tic_tac_toe_test.clj
@@ -0,0 +1,26 @@
+(ns fourclojure.tic-tac-toe-test
+  (:require [clojure.test :refer :all]
+            [fourclojure.tic-tac-toe :refer :all]))
+
+(deftest test-__
+  (is (= nil (__ [[:e :e :e]
+                  [:e :e :e]
+                  [:e :e :e]])))
+  (is (= :x (__ [[:x :e :o]
+                 [:x :e :e]
+                 [:x :e :o]])))
+  (is (= :o (__ [[:e :x :e]
+                 [:o :o :o]
+                 [:x :e :x]])))
+  (is (= nil (__ [[:x :e :o]
+                  [:x :x :e]
+                  [:o :x :o]])))
+  (is (= :x (__ [[:x :e :e]
+                 [:o :x :e]
+                 [:o :e :x]])))
+  (is (= :o (__ [[:x :e :o]
+                 [:x :o :e]
+                 [:o :e :x]])))
+  (is (= nil (__ [[:x :o :x]
+                  [:x :o :x]
+                  [:o :x :o]]))))
