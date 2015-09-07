6b4157f54534dbb3d3a7db9ecfe75b9aa93867cc
Craig Ludington <me@alpheus.me>
2015-09-06 20:48:44 -0500

    start graph-tour

---
+++ src/fourclojure/graph_tour.clj
@@ -0,0 +1,15 @@
+(ns fourclojure.graph-tour)
+
+;; http://www.4clojure.com/problem/89
+
+;; Starting with a graph you must write a function that returns true if
+;; it is possible to make a tour of the graph in which every edge is
+;; visited exactly once.
+
+;; The graph is represented by a vector of tuples, where each tuple represents a single edge.
+
+;; The rules are:
+
+;; - You can start at any node.
+;; - You must visit each edge exactly once.
+;; - All edges are undirected.
---
+++ test/fourclojure/graph_tour_test.clj
@@ -0,0 +1,14 @@
+(ns fourclojure.graph-tour-test
+  (:require [clojure.test :refer :all]
+            [fourclojure.graph-tour :refer :all]))
+
+(deftest test-__
+  (is (= true (__ [[:a :b]])))
+  (is (= false (__ [[:a :a] [:b :b]])))
+  (is (= false (__ [[:a :b] [:a :b] [:a :c] [:c :a]
+                    [:a :d] [:b :d] [:c :d]])))
+  (is (= true (__ [[1 2] [2 3] [3 4] [4 1]])))
+  (is (= true (__ [[:a :b] [:a :c] [:c :b] [:a :e]
+                   [:b :e] [:a :d] [:b :d] [:c :e]
+                   [:d :e] [:c :f] [:d :f]])))
+  (is (= false (__ [[1 2] [2 3] [2 4] [2 5]]))))
