d4e35be3123246c23568df973b6360815aefacf5
Craig Ludington <me@alpheus.me>
2015-09-09 12:12:48 -0500

    game-of-life convert to a singl fn

--- src/fourclojure/game_of_life.clj
+++ src/fourclojure/game_of_life.clj
@@ -1,81 +1,61 @@
 (ns fourclojure.game-of-life)
 
+(def __
+  (fn [world]
+    (letfn [(neighborhood
+              [[x y]]
+              (map (fn vector-add [[xx yy]] (vector (+ x xx) (+ y yy)))
+                   [[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [1 -1] [1 0] [1 1]]))
 
+            (neighbors
+              [world coordinate]
+              (filter world (neighborhood coordinate)))
 
-(defn neighborhood
-  "A vector of neighboring coordinates for a coordinate."
-  [[x y]]
-  (map (fn vector-add [[xx yy]] (vector (+ x xx) (+ y yy)))
-       [[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [1 -1] [1 0] [1 1]]))
+            (pregnant?
+              [world coordinate]
+              (when (and (not (world coordinate))
+                         (= 3 (count (neighbors world coordinate))))
+                coordinate))
 
-(defn neighbors
-  "A vector of (live) cells neighboring a coordinate."
-  [world coordinate]
-  (filter world (neighborhood coordinate)))
+            (spawn
+              [world]
+              (set (filter (partial pregnant? world)
+                           (reduce into [] (map neighborhood world)))))
 
-(defn pregnant?
-  "True if a coordinate has no live cell and has exactly three neighbors."
-  [world coordinate]
-  (when (and (not (world coordinate))
-	     (= 3 (count (neighbors world coordinate))))
-    coordinate))
+            (healthy?
+              [world cell]
+              (when (and (world cell)
+                         (#{2 3} (count (neighbors world cell)))) 
+                cell))
 
-(defn spawn
-  "Return all the new live cells in the world."
-  [world]
-  (set (filter (partial pregnant? world)
-	       (reduce into [] (map neighborhood world)))))
+            (survivors
+              [world]
+              (set (filter (partial healthy? world) world)))
 
-(defn healthy?
-  "Return cell if it is healthy enough to survive."
-  [world cell]
-  (when (and (world cell)
-	     (#{2 3} (count (neighbors world cell)))) 
-    cell))
+            (new-world
+              [world]
+              (into (survivors world) (spawn world)))
 
-(defn survivors
-  "Return all the living cells that survive into the next world."
-  [world]
-  (set (filter (partial healthy? world) world)))
+            (to-coords
+              [world]
+              (set (filter vector?
+                           (reduce into [] (map #(for [x [%] y (range (count (first world)))]
+                                                   (if (= \# (nth (nth world x) y))
+                                                     [x y]))
+                                                (range (count world)))))))
 
-(defn new-world
-  "Return a new world."
-  [world]
-  (into (survivors world) (spawn world)))
+            (from-coords
+              [coords rows cols]
+              (vec (map #(apply str %)
+                        (partition cols (for [x (range rows) y (range cols)]
+                                          (if (coords [x y])
+                                            \#
+                                            \space))))))
+            (main
+              [world]
+              (let [rows (count world)
+                    cols (count (first world))]
+                (from-coords (new-world (to-coords world)) rows cols)))]
 
-
-(def input
-  ["     "
-   "     "
-   " ### "
-   "     "
-   "     "])
-
-(def output
-  ["     "
-   "  #  "
-   "  #  "
-   "  #  "
-   "     "])
-(defn to-coords
-  [world]
-  (set (filter vector?
-               (reduce into [] (map #(for [x [%] y (range (count (first world)))]
-                                       (if (= \# (nth (nth world x) y))
-                                         [x y]))
-                                    (range (count world)))))))
-
-(defn from-coords
-  [coords rows cols]
-  (vec (map #(apply str %)
-            (partition cols (for [x (range rows) y (range cols)]
-                              (if (coords [x y])
-                                \#
-                                \space))))))
-
-(defn __
-  [world]
-  (let [rows (count world)
-        cols (count (first world))]
-    (from-coords (new-world (to-coords world)) rows cols)))
+      (main world))))
 
