59bf371aba535d1a2c63b3795e9570a16aa83e20
Craig Ludington <me@alpheus.me>
2015-09-09 12:22:35 -0500

    win-tic-tac-toe convert to single fn

--- src/fourclojure/win_tic_tac_toe.clj
+++ src/fourclojure/win_tic_tac_toe.clj
@@ -14,23 +14,25 @@
 ;; Board coordinates should be as in calls to get-in. For example, [0 1]
 ;; is the topmost row, center position.
 
-;; unoccupied cells on the board
-(defn available [board] (for [x (range 3) y (range 3) :when (= :e (get-in board [x y]))] [x y]))
+(def __ 
+  (fn [piece board]
+    (letfn [;; unoccupied cells on the board
+            (available [board] (for [x (range 3) y (range 3) :when (= :e (get-in board [x y]))] [x y]))
 
-(defn make-diag [board diagonal xy] (if ((set diagonal) xy) (map #(get-in board %) diagonal) []))
-(defn row [board [x y]] (nth board x))
-(defn column [board [x y]] (map #(nth % y) board))
-(defn up-diag [board xy] (make-diagonal board [[0 0] [1 1] [2 2]] xy))
-(defn down-diag [board xy] (make-diagonal board [[2 0] [1 1] [0 2]] xy))
+            (make-diagonal [board diagonal xy] (if ((set diagonal) xy) (map #(get-in board %) diagonal) []))
+            (row [board [x y]] (nth board x))
+            (column [board [x y]] (map #(nth % y) board))
+            (up-diag [board xy] (make-diagonal board [[0 0] [1 1] [2 2]] xy))
+            (down-diag [board xy] (make-diagonal board [[2 0] [1 1] [0 2]] xy))
 
-;; every row, column, or diagonal containing this xy coordinate
-(defn containers [board xy] (remove empty? [(row board xy) (column board xy) (up-diag board xy) (down-diag board xy)]))
+            ;; every row, column, or diagonal containing this xy coordinate
+            (containers [board xy] (remove empty? [(row board xy) (column board xy) (up-diag board xy) (down-diag board xy)]))
 
-;; test that a row, column, or diagonal wins for the given playing piece
-(defn rcd-wins? [rcd piece] (every? #(= piece %) rcd))
+            ;; test that a row, column, or diagonal wins for the given playing piece
+            (rcd-wins? [rcd piece] (every? #(= piece %) rcd))
 
-(defn play-wins? [board piece xy] (some #(rcd-wins? % piece) (containers (assoc-in board xy piece) xy)))
+            (play-wins? [board piece xy] (some #(rcd-wins? % piece) (containers (assoc-in board xy piece) xy)))
 
-(defn winning-moves [piece board] (set (filter #(play-wins? board piece %) (available board))))
-
-(def __ winning-moves)
+            (winning-moves [piece board] (set (filter #(play-wins? board piece %) (available board))))
+            ]
+      (winning-moves piece board))))
