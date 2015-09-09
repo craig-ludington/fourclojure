ad68ceb79d4aa8b02de0855e7943ce86037a40c0
Craig Ludington <me@alpheus.me>
2015-09-09 12:28:52 -0500

    transitive-closure tests

---
+++ src/fourclojure/transitive_closure.clj
@@ -0,0 +1,12 @@
+(ns fourclojure.transitive-closure
+  (:use [clojure.pprint :refer :all]
+         [clojure.repl :refer :all]))
+
+;; http://www.4clojure.com/problem/84
+
+
+;; Write a function which generates the transitive closure of a binary
+;; relation. The relation will be represented as a set of 2 item vectors.
+
+(def __
+  (fn [s]))
