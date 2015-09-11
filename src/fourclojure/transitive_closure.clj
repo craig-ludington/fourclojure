13d7856f23a2a49ebb797b8371e2f3faf76af9ec
Craig Ludington <me@alpheus.me>
2015-09-11 10:11:27 -0500

    transitive-closure passes tests

--- src/fourclojure/transitive_closure.clj
+++ src/fourclojure/transitive_closure.clj
@@ -4,9 +4,20 @@
 
 ;; http://www.4clojure.com/problem/84
 
-
 ;; Write a function which generates the transitive closure of a binary
 ;; relation. The relation will be represented as a set of 2 item vectors.
 
-(def __
-  (fn [s]))
+(def __ 
+  (fn [tuples]
+    (letfn [(thread [tuples start]
+                    (loop [m (into {} tuples)
+                           acc []
+                           point start]
+                      (if point
+                        (recur m (conj acc point) (m point))
+                        acc)))
+
+            (threads [tuples] (map #(thread tuples (first %)) tuples))
+            (closures [v] (map (partial vector (first v)) (rest v)))
+            (transitive-closures [tuples] (reduce into #{} (map closures (threads tuples))))]
+      (transitive-closures tuples))))
